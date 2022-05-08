c69529eab58029099c632c91bdd4e348ab3a0d40
Mahyar <shakourimahyar@outlook.com>
2022-05-08 13:12:59 +0430

    Initial Commit

---
+++ SimpleSwiftUIProject/SimpleSwiftUIProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleSwiftUIProjectApp.swift
+//  SimpleSwiftUIProject
+//
+//  Created by MahyarShakouri on 5/8/22.
+//
+
+import SwiftUI
+
+@main
+struct SimpleSwiftUIProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
